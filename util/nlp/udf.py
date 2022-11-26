eb36b8fc93217a56b5bcb164f5ea6ec2e5a61bf8
Anton Kozyriev <sonara4mus@gmail.com>
2022-11-26 16:25:37 +0200

    feat(util.nlp.udf): UDF for full text processing pipeline

--- util/nlp/udf.py
+++ util/nlp/udf.py
@@ -24,7 +24,7 @@ def sub_punc(text: str, sub_token=' ') -> str:
     # Substitute punctuation
     return re.sub(r'[!"#$%&\(\)\*\+,\./:;<=>?@\\^_`{|}~\[\]-]+', sub_token, text)
 
-def sub_num_words(text: str, sub_token='') -> str:
+def sub_numwords(text: str, sub_token='') -> str:
     # Substitute words that contain digits
     return re.sub(r'\b(\w)*(\d)(\w)*\b', sub_token, text)
 
@@ -32,22 +32,38 @@ def sub_space(text: str, sub_token=' ') -> str:
     # Remove redundant spaces between each word
     return re.sub(r'[ ]+', sub_token, text)
 
-def social_proc(text: str) -> str:
-    # Preprocess text from a social media
-    steps = [
-        lambda text: text.lower(),
-        lambda text: sub_usertags(text),
-        lambda text: sub_hashtags(text),
-        lambda text: sub_sepr(text),
-        lambda text: sub_num_words(text),
-        lambda text: sub_space(text),
-        lambda text: text.strip()
-    ]
-
+def iter_proc(text: str, steps=[]) -> str:
+    # Iterative preprocessing of a text data
     for step in steps:
         text = step(text)
 
     return text
+
+def social_proc(text: str) -> str:
+    # Preprocess text from a social media
+    return iter_proc(text, steps=[
+        lambda text: text.lower(),
+        lambda text: sub_usertags(text),
+        lambda text: sub_hashtags(text),
+        lambda text: sub_sepr(text),
+        lambda text: sub_numwords(text),
+        lambda text: sub_space(text),
+        lambda text: text.strip()
+    ])
+
+def full_proc(text: str) -> str:
+    # Preprocess text with all available pipelines
+    return iter_proc(text, steps=[
+        lambda text: text.lower(),
+        lambda text: sub_usertags(text),
+        lambda text: sub_hashtags(text),
+        lambda text: sub_unicode(text),
+        lambda text: sub_sepr(text),
+        lambda text: sub_punc(text),
+        lambda text: sub_numwords(text),
+        lambda text: sub_space(text),
+        lambda text: text.strip()
+    ])
 
 
 # Preprocessing UDFs
@@ -58,7 +74,7 @@ sub_hashtags_udf = udf(lambda text: sub_hashtags(text), StringType())
 sub_unicode_udf = udf(lambda text: sub_unicode(text), StringType())
 sub_sepr_udf = udf(lambda text: sub_sepr(text), StringType())
 sub_punc_udf = udf(lambda text: sub_punc(text), StringType())
-sub_num_words_udf = udf(lambda text: sub_num_words(text), StringType())
+sub_numwords_udf = udf(lambda text: sub_numwords(text), StringType())
 sub_space_udf = udf(lambda text: sub_space(text), StringType())
 
 # Metrics UDFs
@@ -67,3 +83,4 @@ charlen_udf = udf(lambda text: len(text), IntegerType())
 
 # Custom UDFs
 social_proc_udf = udf(lambda text: social_proc(text), StringType())
+full_proc_udf = udf(lambda text: full_proc(text), StringType())
