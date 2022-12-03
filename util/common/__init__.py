5ff0b8ff8700eb99f40c274968371264b67678e4
Anton Kozyriev <sonara4mus@gmail.com>
2022-12-04 00:54:48 +0200

    feat(util.common): value mapping procedures

--- util/common/__init__.py
+++ util/common/__init__.py
@@ -1,1 +1,2 @@
-from .proc import shuffle_df, clean_df
+from .proc import shuffle_df, clean_df, glue_cols, map_cols
+from .udf import cols_to_arr_udf, cols_to_dict_udf
--- util/common/proc.py
+++ util/common/proc.py
@@ -1,11 +1,20 @@
 from pyspark.sql import DataFrame
-from pyspark.sql.functions import rand
+from pyspark.sql.functions import rand, array
+from .udf import cols_to_arr_udf, cols_to_dict_udf
 
 
 def shuffle_df(df: DataFrame, seed=None) -> DataFrame:
-    # Return a DataFrame with randomly shuffled rows
+    """Return a DataFrame with randomly shuffled rows."""
     return df.orderBy(rand(seed=seed))
 
 def clean_df(df: DataFrame, columns: list) -> DataFrame:
-    # Basic cleaning operation (drop NULLs and duplicates) on selected columns
+    """Basic cleaning operation (drop NULLs and duplicates) on selected columns."""
     return df.dropDuplicates(columns).dropna(subset=tuple(columns))
+
+def glue_cols(df: DataFrame, cols: list, output_col: str) -> DataFrame:
+    """Joins dataframe columns `cols` into an array-column as `output_col`."""
+    return df.select(cols_to_arr_udf(array(*cols)).alias(output_col))
+
+def map_cols(df: DataFrame, key_col: str, val_col: str, output_col: str) -> DataFrame:
+    """Joins dataframe columns into a map-column `output_col`."""
+    return df.select(cols_to_dict_udf(array(key_col, val_col)).alias(output_col))
