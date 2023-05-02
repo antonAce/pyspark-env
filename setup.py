980da2b5cffeb020a63583b7f19332c42163c4f4
Anton Kozyriev <sonara4mus@gmail.com>
2023-05-02 15:57:25 +0300

    fix: source code and notebooks structure

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+
+setup(
+    name="Environment setup for Apache Spark",
+    version="1.0",
+    description="A personal environment setup for Apache Spark scripting."
+)
+
